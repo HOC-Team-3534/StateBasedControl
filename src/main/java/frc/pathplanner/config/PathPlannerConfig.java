9812efb586777c74d85c5671f0f7096a6e5a093e
Malachi <malachijb28@gmail.com>
2023-01-21 13:04:46 -0500

    dist correction can't cast. also tuned the find closest point method

--- src/main/java/frc/pathplanner/config/PathPlannerConfig.java
+++ src/main/java/frc/pathplanner/config/PathPlannerConfig.java
@@ -5,6 +5,5 @@ package frc.pathplanner.config;
  */
 public class PathPlannerConfig {
     public static double LOOK_AHEAD_TIME = 0.020;
-    public static double RESET_WINDOW_SIZE = 1.0;
     public static boolean USE_DIST_CORRECTION_MODE = false;
 }
